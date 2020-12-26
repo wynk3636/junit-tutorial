bd152ca8bae4de6623d243620e750747849122e5
ishida <h_hand_3636@gmail.com>
2020-12-26 23:43:36 +0900

    first test

---
+++ src/main/Calculator.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Calculator {
+	
+	public int multiply(int x, int y) {
+		return x * y;
+	}
+	
+	public int divide(int x, int y) {
+		return x / y;
+	}
+
+}
---
+++ src/test/CalculatorTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import main.Calculator;
+
+public class CalculatorTest {
+	
+	@Test
+	public void test() {
+		fail("not create");
+	}
+	
+	@Test
+	public void multiplyTest() {
+		Calculator calc = new Calculator();
+		
+		int expected = 12;
+		int actual = calc.multiply(3, 4);
+		
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void multiplyTestError() {
+		Calculator calc = new Calculator();
+		
+		int expected = 12;
+		int actual = calc.multiply(5, 7);
+		
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void divideTest() {
+		Calculator calc = new Calculator();
+		
+		float expected = 1.5f;
+		float actual = calc.divide(3, 2);
+		
+		assertThat(actual, is(expected));
+	}
+}
